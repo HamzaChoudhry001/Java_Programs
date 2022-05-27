d9968c59bf1f1158cfa52c2ec8cb54fbe1c235c1
HamzaChoudhry001 <hamzapervez82@gmail.com>
2022-05-27 06:32:18 -0700

    introbasic

---
+++ intro.java
@@ -0,0 +1,37 @@
+public class intro {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.console().readLine();// for input,
+
+        //readLine() it can use for any input to read from console. it will return user input in string
+        
+        //preemtive datatypes: those which language  provide by default: etc. int float decimal double char bool,  
+        /* 
+        preemtivetypes are structures. but java suggest us to  or use objects. 
+        agr hum chaty hain k int structure ki bjye int object use kry tu os liye etc.
+        Integer i = 10;//ye object bn gya.
+        i.notifyAll(); //notifyAll() ye aik function hai, jis ko i sy access kiya hai
+        object: "jis k ander functions / properties ho gi" jiny hum access kr sakty hain*/
+    
+
+        //non-prremtive datatyps: Are those int which language use additional classes like arrays,classes
+        //jo k language ny additional classes use kr k implement ki hoti hain like arrays,classes,string.
+    }
+}
+
+
+
+
+//javac intro.java , here javac is compiler, or javac is execuper of that program for run.
+
+//Q:java code run command?
+//ans: javac filename.java
+//then for run our java code, we do-->  java filename
+
+//Q:how java code compile?
+//ans: phly os ka bit-code ya bitr-file banta hai, then wo compile hota.
+
+//bit-code(ye low level set of instruction ho gi/ optimize instruction hon gi)----->   filename.class wali file hoti hai
+
+//note:
+//if we have .class file then we can do reverse engineering means we can produce form bite-code to actual-code .java file code using different editors.
