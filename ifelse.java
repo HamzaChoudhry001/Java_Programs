6cbb937a25f617902a563068b57eca59972d7033
HamzaChoudhry001 <hamzapervez82@gmail.com>
2022-05-27 03:27:34 -0700

    ifelse statement

---
+++ ifelse.java
@@ -0,0 +1,26 @@
+public class ifelse {
+    public static void main(String[] args){
+        System.out.print("Enter 1st value:"); 
+        String a1 = System.console().readLine();
+
+        System.out.print("Enter 2nd value :"); 
+        String b1 = System.console().readLine();
+
+        System.out.print("Enter 3rd value :"); 
+        String c1 = System.console().readLine();
+
+        Integer a = Integer.parseInt(a1);
+        Integer b = Integer.parseInt(b1);
+        Integer c = Integer.parseInt(c1);
+
+        if(a > b && a > c){
+            System.out.print("a is greater"); 
+        }
+        else if(b > a && b > c){
+            System.out.print("b is greater"); 
+        }
+        else{
+            System.out.print("c is greater"); 
+        }
+    }
+}
