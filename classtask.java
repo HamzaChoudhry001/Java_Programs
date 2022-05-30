f58b15eedfb4152beba57d8839ceb033a9180458
HamzaChoudhry001 <hamzapervez82@gmail.com>
2022-05-30 11:16:27 -0700

    Task1Grade

---
+++ classtask.java
@@ -0,0 +1,33 @@
+public class classtask {
+
+    public static void Grademarks(){
+        System.out.print("Enter marks :"); 
+        String marks = System.console().readLine();
+
+        Integer a = Integer.parseInt(marks);
+
+        if(a >= 80){
+            System.out.println("A Grade"); 
+        }
+        else if(a >=70 && a < 80){
+            System.out.println("B Grade"); 
+        }
+        else if(a >=60 && a < 70){
+            System.out.println("C Grade"); 
+        }
+        else if(a >=50 && a < 60){
+            System.out.println("D Grade"); 
+        }
+        else{
+            System.out.println("F Grade"); 
+        }
+    }
+      public static void main(String[] args){
+        Grademarks();
+        Grademarks();
+
+      }
+   
+        
+       
+}
