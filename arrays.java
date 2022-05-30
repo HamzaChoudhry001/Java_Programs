3a6858d5fea513bc0b1f3b4e48e830575366dc5d
HamzaChoudhry001 <hamzapervez82@gmail.com>
2022-05-30 11:16:06 -0700

    arrayBasic

---
+++ arrays.java
@@ -0,0 +1,47 @@
+public class arrays {
+    public static void main(String[] args) {
+
+
+
+    // int[] array = new int[2];
+    // array[0] = 10;
+    // array[1] = 20;
+    
+   // int[] array = {0,1};
+
+
+
+   /*      // String[] cas; // To declare an array
+    //int[] myNum = {10, 20, 30, 40};// To create an array of integers,
+
+    //Access the Elements of an Array
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    System.out.println(cars[0]);//vovlo
+
+    //Change an Array Element
+    cars[0] = "Honda";
+    System.out.println(cars[0]);// Now outputs Opel instead of Volvo
+       
+//Loop Through an Array
+       String[] car = {"Volvo", "BMW", "Ford", "Mazda"};
+       for (int i = 0; i < car.length; i++) {
+         System.out.println(car[i]);
+       }
+    
+        
+    }*/
+
+    // Loop Through an Array with For-Each
+    /*Syntax
+    for (type variable : arrayname) {
+        ...
+      }*/
+      String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars) {
+        System.out.println(i);
+        }
+}
+  
+}
+
+
